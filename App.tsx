74649c27c930a25a459c91bc97c5c3ea1a972625
Blerr511 <lyovghukasyan@gmail.com>
2021-04-03 00:10:54 +0400

    added initial routing

--- App.tsx
+++ App.tsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
-import {Provider, useDispatch} from 'react-redux';
-import actions from 'store/actions';
+import {Provider} from 'react-redux';
 import store from 'store';
+import MainView from 'Views';
 
 const App = () => {
-	const dispatch = useDispatch();
-	const handlePress = () => {
-		console.log('press');
-		dispatch(actions.auth.signIn({email: 'test@test.test', password: 'testtest'}));
-	};
-
 	return (
-		<TouchableOpacity
-			activeOpacity={0.5}
-			onPress={handlePress}
-			style={{backgroundColor: 'red', padding: 50, zIndex: 22}}
-			hitSlop={{top: 30, bottom: 30, left: 30, right: 30}}>
-			<Text>Click Me!</Text>
-		</TouchableOpacity>
+		<Provider store={store}>
+			<MainView />
+		</Provider>
 	);
 };
 
-const AppProvider = () => (
-	<Provider store={store}>
-		<App />
-	</Provider>
-);
-
-export default AppProvider;
+export default App;
