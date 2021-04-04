1ed5d24b130d0cb6533399f2be8f2297da36e4a2
Blerr511 <lyovghukasyan@gmail.com>
2021-04-05 00:34:01 +0400

    added type prop on Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,26 @@
 import {createStyleSheet} from 'hooks/useTheme';
-import React, {ReactNode} from 'react';
+import React, {ReactNode, useMemo} from 'react';
 import {StyleProp, Text, TextStyle, TouchableOpacity, TouchableOpacityProps} from 'react-native';
 
 const useStyles = createStyleSheet(theme => ({
 	container: {
+		backgroundColor: 'transparent',
 		paddingHorizontal: theme.spacing(2),
 		paddingVertical: theme.spacing(1),
-		borderRadius: theme.shape.borderRadius,
+		borderRadius: theme.shape.borderRadius
+	},
+	bordered: {
+		borderWidth: 2,
+		borderColor: theme.pallet.primary
+	},
+	filled: {
 		backgroundColor: theme.pallet.primary
 	},
+	filledText: {
+		color: theme.typography.color.secondary
+	},
 	text: {
-		color: theme.typography.color.secondary,
+		color: theme.pallet.primary,
 		fontSize: theme.typography.fontSize.medium,
 		textAlign: 'center'
 	}
@@ -20,14 +30,19 @@ export interface ButtonProps extends TouchableOpacityProps {
 	text?: string;
 	textStyles?: StyleProp<TextStyle>;
 	children?: ReactNode;
+	type?: 'filled' | 'text' | 'bordered';
 }
 
-const Button = ({text, style, textStyles, children, ...props}: ButtonProps) => {
+const Button = ({text, style, textStyles, type = 'filled', children, ...props}: ButtonProps) => {
 	const styles = useStyles();
+	const filled = type === 'filled';
+	const bordered = type === 'bordered';
 
 	return (
-		<TouchableOpacity style={style ? [styles.container, styles.container] : styles.container} {...props}>
-			<Text style={textStyles ? [styles.text, textStyles] : styles.text}>{text}</Text>
+		<TouchableOpacity
+			style={[styles.container, filled && styles.filled, bordered && styles.bordered, style]}
+			{...props}>
+			<Text style={[styles.text, filled && styles.filledText, textStyles]}>{text}</Text>
 			{children}
 		</TouchableOpacity>
 	);
