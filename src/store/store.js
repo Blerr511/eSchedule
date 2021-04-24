8b5a8bcd89d56247110a317c29f141bb1266eab3
Blerr511 <lyovghukasyan@gmail.com>
2021-04-25 00:21:24 +0400

    auth / removed unused store.js

--- src/store/store.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit';
-import reducers from './reducers';
-
-const store = configureStore({reducer: combineReducers(reducers)});
-
-export default store;
