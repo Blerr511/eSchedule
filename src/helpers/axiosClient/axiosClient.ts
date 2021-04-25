2aa1d33a1b52376f008dcff41146c2849b247cd4
Blerr511 <lyovghukasyan@gmail.com>
2021-04-25 17:22:45 +0400

    added axiosClient

---
+++ src/helpers/axiosClient/axiosClient.ts
@@ -0,0 +1,20 @@
+import {API_URL} from '@env';
+import {firebase} from '@react-native-firebase/auth';
+import axios from 'axios';
+
+const axiosClient = axios.create({baseURL: API_URL});
+
+axiosClient.interceptors.request.use(
+	async config => {
+		const user = firebase.auth().currentUser;
+		if (!user) return config;
+		const token = await user.getIdToken();
+		config.headers.authorization = `Bearer ${token}`;
+		return config;
+	},
+	error => {
+		return Promise.reject(error);
+	}
+);
+
+export default axiosClient;
