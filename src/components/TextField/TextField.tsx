b43421950b9fede7354f5c3aac8efc0f67be08ec
Blerr511 <lyovghukasyan@gmail.com>
2021-04-29 03:27:57 +0400

    TextField style fixes

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import {createStyleSheet} from 'hooks';
 import {Input, InputProps} from 'react-native-elements';
 
 export interface TextFieldProps extends InputProps {}
 
+type InputStyles = Pick<
+	TextFieldProps,
+	| 'style'
+	| 'errorStyle'
+	| 'inputStyle'
+	| 'labelStyle'
+	| 'containerStyle'
+	| 'style'
+	| 'disabledInputStyle'
+	| 'inputContainerStyle'
+	| 'leftIconContainerStyle'
+	| 'rightIconContainerStyle'
+>;
+
+const useStyles = createStyleSheet<InputStyles>(theme => ({
+	inputStyle: {
+		color: theme.typography.color.primary
+	},
+	inputContainerStyle: {
+		borderColor: theme.pallet.primary
+	},
+	labelStyle: {
+		color: theme.pallet.primary
+	}
+}));
+
 const TextField = ({...props}: TextFieldProps, ref: React.ForwardedRef<Input>) => {
-	return <Input {...props} ref={ref} />;
+	const styles = useStyles();
+	return <Input {...styles} {...props} ref={ref} />;
 };
 
 export default React.forwardRef(TextField);
